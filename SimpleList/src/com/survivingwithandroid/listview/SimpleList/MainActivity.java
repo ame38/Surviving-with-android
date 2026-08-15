61a9346936512f34463feece634b31954f5d50cd
ame38 <8823178+ame38@users.noreply.github.com>
2026-08-15 17:24:24 +0000

    fix typo contextmneu -> context menu in comments

--- SimpleList/src/com/survivingwithandroid/listview/SimpleList/MainActivity.java
+++ SimpleList/src/com/survivingwithandroid/listview/SimpleList/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends Activity {
 			}
 		   });
         
-          // we register for the contextmneu        
+          // we register for the context menu
           registerForContextMenu(lv);
         }
    
